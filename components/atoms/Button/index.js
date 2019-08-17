7fba1a73d4fa851b7c5736a2b8de491bdcef7f53
Philliph Drummond <oilsinwater@gmail.com>
2019-08-17 03:46:06 -0700

    bug: error blocks build process

--- components/atoms/Button/index.js
+++ components/atoms/Button/index.js
@@ -1,15 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button as MaterialButton } from '@material-ui/core';
 
-const Button = props => {
+const Button = (props) => {
   const { children, ...defaultProps } = props;
 
   return <MaterialButton {...defaultProps}>{children}</MaterialButton>;
 };
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default Button;
